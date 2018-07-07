e694e14ed45858838a2efabf1a1a81a9a188bba4
Tenebrisse <quentinhermiteau@gmail.com>
2018-07-07 12:19:00 +0200

    :sparkles: generate ApiController for generation of documentation

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Class ApiController
+ *
+ * @package App\Http\Controllers
+ *
+ * @SWG\Swagger(
+ *     basePath="",
+ *     host="127.0.0.1:8000",
+ *     schemes={"http"},
+ *     @SWG\Info(
+ *         version="1.0",
+ *         title="API",
+ *     ),
+ *     @SWG\Definition(
+ *         definition="Error",
+ *         required={"code", "message"},
+ *         @SWG\Property(
+ *             property="code",
+ *             type="integer",
+ *             format="int32"
+ *         ),
+ *         @SWG\Property(
+ *             property="message",
+ *             type="string"
+ *         )
+ *     )
+ * )
+ */
+class ApiController extends Controller
+{
+    //
+}
